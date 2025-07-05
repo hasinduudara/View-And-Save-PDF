71c52686415e6e28200b25c191a2c299c3a34093
Hasindu Udara <hasindutechschool@gmail.com>
2025-07-05 10:25:49 +0530

    create main page

--- src/main/java/lk/hasindu/viewandsavepdf/controller/MainPageController.java
+++ src/main/java/lk/hasindu/viewandsavepdf/controller/MainPageController.java
@@ -1,4 +1,37 @@
 package lk.hasindu.viewandsavepdf.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainPageController {
+
+    @FXML
+    private Button btnPreview;
+
+    @FXML
+    private Button btnSavePDF;
+
+    @FXML
+    private AnchorPane mainPage;
+
+    @FXML
+    private TextArea txtMailDescription;
+
+    @FXML
+    private TextField txtMailTitle;
+
+    @FXML
+    void btnPreviewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSavePDFOnAction(ActionEvent event) {
+
+    }
+
 }
